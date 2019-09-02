4a0b1dedd68ebb7cbb3b6b8d7d117f07de170fe8
Freya <Freya@surfaceBro>
2019-09-02 12:28:03 -0400

    test1

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,6 +1,7 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class A1Jedi {
 
@@ -10,5 +11,58 @@ public class A1Jedi {
 
 		// Your code follows here.
 		
+		int totalitems= scan.nextInt();
+		String[] NameArr= new String[totalitems];
+		
+		for (int i = 0; i<totalitems; i++) {
+			String ItemName= scan.next();
+			NameArr[i] = ItemName;
+				
+			Double ItemPrice = scan.nextDouble();
+			Double[] PriceArr= new Double[totalitems];
+			PriceArr[i] = ItemPrice;
+			
+			System.out.println(ItemName);
+			System.out.println(ItemPrice);
+			}
+		
+		int numberofcustomers = scan.nextInt();
+
+			for (int j=0; j<numberofcustomers; j++) {
+				String FirstName = scan.next();
+				String LastName = scan.next();
+				int ItemNumber = scan.nextInt();
+				
+				//int[] amounts = new int[ItemNumber];
+				//String[] items = new String[ItemNumber];
+				
+				int[] totalamounts = new int[totalitems];
+				int[] totalcustomers = new int[totalitems];
+				
+				for(int k=0; k<ItemNumber; k++) {
+					
+					int Amount = scan.nextInt();
+					//amounts[k]= Amount;
+					
+					String Item = scan.next();
+					//items[k]= Item;
+					
+					int index= Arrays.binarySearch(NameArr, Item);
+					totalamounts[index] += Amount;
+					
+					if (Amount>0) {
+						totalcustomers[index] += 1;
+					}
+
+					}
+				if (j== numberofcustomers-1) {
+					for (int r = 0; r<totalitems; r++) {
+						System.out.println(totalcustomers[r]+" customers bought "+totalamounts[r]+" "+NameArr[r]);
+				}
+			
+		}
+			
+					
+				}
 	}
 }
