70c802b710c2b639c5b2f7fd2d75084fc8c521d4
Freya <Freya@surfaceBro>
2019-09-02 04:00:28 -0400

    adept1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -10,5 +10,36 @@ public class A1Adept {
 
 		// Your code follows here.
 		
+		
+		int totalitems= scan.nextInt();
+		for (int i = 0; i<totalitems; i++) {
+			String ItemName= scan.next();
+			String[] NameArr= new String[totalitems];
+			NameArr[i] = ItemName;
+				
+			Double ItemPrice = scan.nextDouble();
+			Double[] PriceArr= new Double[totalitems];
+			PriceArr[i] = ItemPrice;
+			
+			System.out.println(ItemName);
+			System.out.println(ItemPrice);
+			}
+		
+		int numberofcustomers = scan.nextInt();
+		for (int j=0; j<numberofcustomers; j++) {
+			String FirstName = scan.next();
+			String LastName = scan.next();
+			int ItemNumber = scan.nextInt();
+			
+			for(int k=0; k<ItemNumber; k++) {
+				int Amount = scan.nextInt();
+				String Item = scan.next();
+				
+				System.out.println(FirstName+LastName);
+			}
+		}
+		
+		
+		
 	}
 }
