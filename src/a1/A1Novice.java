21ebf8130d3657a071009af8c545156e1564f731
Freya <Freya@surfaceBro>
2019-09-02 01:57:33 -0400

    check2

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -31,7 +31,7 @@ public class A1Novice {
 				double[] totaling = new double[itemtypes];
 				totaling[j] = totalitem;
 				
-				if (j == itemtypes-1);		
+				if (j > itemtypes-1);		
 					System.out.println(firstinitial+". "+lastname+": "+totalitem);
 				
 				
