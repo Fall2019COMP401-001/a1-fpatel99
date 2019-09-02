5426a60c703809c2d82d0de2cfc0e3abfa538064
Freya <Freya@surfaceBro>
2019-09-01 23:02:26 -0400

    test

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -11,14 +11,12 @@ public class A1Novice {
 
 		// Your code follows here.
 		
-	    int totalcustomers = scan.nextInt();// Read user input
+	    String totalcustomers = scan.next();// Read user input
 	    System.out.println(totalcustomers);
 
+
 	    
-	    int[] numbers = new int[totalcustomers];
-	    System.out.println(numbers);
-	    
-	    for (int i = 0; i <= totalcustomers; i++) {
+	    //for (int i = 0; i <= totalcustomers; i++) {
 	    	
 	    	int items = 0;
 	    	
@@ -32,4 +30,4 @@ public class A1Novice {
 	    }
 		
 	}
-}
+//}
