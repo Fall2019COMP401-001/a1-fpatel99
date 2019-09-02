fa4bfe391defd27b362820ac062b786dfa998cd6
Freya <Freya@surfaceBro>
2019-09-01 22:25:26 -0400

    check

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,21 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		
 
 		// Your code follows here.
+		
+		System.out.println("Enter Total Number of Customers");
+	    int totalcustomers = scan.nextInt();// Read user input
+	    System.out.println(totalcustomers);
+	    
+	    for (int i = 0; i <= totalcustomers; i++) {
+	    	
+	    	System.out.println("Enter Customer Name");
+	    	
+	    
+	    
+	    }
 		
 	}
 }
